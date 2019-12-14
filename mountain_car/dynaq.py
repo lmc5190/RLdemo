7d4ccbaef1ac92a1aafd7e7071bd97f0c336af66
Landon Chambers <landon_chambers@SAAS.LOCAL@gpu2.saas.local>
2019-12-14 02:04:52 -0600

    implemented dynasarsa gym-maze-myscripts

---
+++ mountain_car/dynaq.py
@@ -0,0 +1,126 @@
+import numpy as np
+import gym
+import matplotlib.pyplot as plt
+import time
+import random
+
+# Import and initialize Mountain Car Environment
+env = gym.make('MountainCar-v0')
+env.reset()
+
+# Define Q-learning function
+def QLearning(env, discount, alpha,  min_alpha,  epsilon, min_epsilon, episodes, episodes_stop_exploring, planning_steps):
+    # Determine size of discretized state space
+    num_states = (env.observation_space.high - env.observation_space.low)*\
+                    np.array([10, 100])
+    num_states = np.round(num_states, 0).astype(int) + 1
+    
+    # Initialize Q table
+    Q = np.random.uniform(low = -1, high = 1, 
+                          size = (num_states[0], num_states[1], 
+                                  env.action_space.n))
+    
+    #Environment Model
+    env_model = []
+    
+    # Initialize variables to track rewards
+    reward_list = []
+    ave_reward_list = []
+
+    #compute alpha and epsilon decay
+    decay_epsilon = (epsilon - min_epsilon)/episodes_stop_exploring
+    decay_alpha = (alpha - min_alpha)/episodes
+    
+    # Run Q learning algorithm
+    for i in range(episodes):
+        # Initialize parameters
+        done = False
+        tot_reward, reward = 0,0
+        state = env.reset()
+        
+        # Discretize state
+        state_adj = (state - env.observation_space.low)*np.array([10, 100])
+        state_adj = np.round(state_adj, 0).astype(int)
+    
+        while done != True:   
+            # Render environment for last five episodes
+            if i >= (episodes - 5):
+                env.render()
+                time.sleep(0.050)
+                
+            # Determine next action - epsilon greedy strategy
+            if np.random.random() < 1 - epsilon:
+                action = np.argmax(Q[state_adj[0], state_adj[1]]) 
+            else:
+                action = np.random.randint(0, env.action_space.n)
+                
+            # Get next state and reward
+            state2, reward, done, info = env.step(action) 
+            
+            # Discretize state2
+            state2_adj = (state2 - env.observation_space.low)*np.array([10, 100])
+            state2_adj = np.round(state2_adj, 0).astype(int)
+            
+            #Update environment model using experience
+            env_model.append([state_adj, action, reward,state2_adj])
+
+            #Allow for terminal states
+            if done and state2[0] >= 0.5:
+                Q[state_adj[0], state_adj[1], action] = reward
+                
+            # Adjust Q value for current state
+            else:
+                delta = alpha*(reward + 
+                                 discount*np.max(Q[state2_adj[0], 
+                                                   state2_adj[1]]) - 
+                                 Q[state_adj[0], state_adj[1],action])
+                Q[state_adj[0], state_adj[1],action] += delta
+
+                #add planning
+                for n in range(planning_steps):
+                    s,a,r,sp = random.choice(env_model)
+                    delta = alpha*(r + 
+                                 discount*np.max(Q[sp[0], 
+                                                   sp[1]]) - 
+                                 Q[s[0], s[1],a])
+                    Q[s[0], s[1],a] += delta
+                                                         
+            # Update variables
+            tot_reward += reward
+            state_adj = state2_adj
+        
+        # Decay epsilon
+        if epsilon > min_epsilon:
+            epsilon -= decay_epsilon
+
+        # Decay alpha
+        if alpha > min_alpha:
+            alpha -= decay_alpha
+
+        # Track rewards
+        reward_list.append(tot_reward)
+        
+        if (i+1) % 100 == 0:
+            ave_reward = np.mean(reward_list)
+            ave_reward_list.append(ave_reward)
+            reward_list = []
+            
+        if (i+1) % 100 == 0:    
+            print('Episode {} Average Reward: {} Alpha: {} Epsilon {}'.format(i+1, ave_reward, alpha, epsilon))
+            
+    env.close()
+    
+    return ave_reward_list
+
+# Run Q-learning algorithm
+rewards = QLearning(env, 0.9, 0.1, 0.001, 0.15, 0.005, 20000, 5000, 40)
+#def QLearning(env, discount, alpha,  min_alpha, epsilon, min_epsilon, episodes, episodes_stop_exploring, planning_steps):
+
+# Plot Rewards
+plt.plot(100*(np.arange(len(rewards)) + 1), rewards)
+plt.xlabel('Episodes')
+plt.ylabel('Average Reward')
+plt.title('Average Reward vs Episodes')
+plt.savefig('dynaq_rewards.png')
+plt.show()     
+plt.close()  
