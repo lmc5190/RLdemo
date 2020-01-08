3529dcda612074dc1f6bfdab8cd898737b343e0a
Landon Chambers <landon_chambers@SAAS.LOCAL@gpu2.saas.local>
2020-01-07 18:19:24 -0600

    added plots for n

--- experiments/dynasarsa_vs_nstepsarsa/n/plot.py
+++ experiments/dynasarsa_vs_nstepsarsa/n/plot.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 def compute_meansandstderrors_overruns(df, metric):
     #returns dataframe with means and standard errors over runs for specified metric
     #grouping done by methods and decay_multiplier
-    df_wmeans = df.groupby(['method','decay_multiplier'])[metric].mean().to_frame(name = 'mean_'+ metric).reset_index()
-    df_wsems = df.groupby(['method', 'decay_multiplier'])[metric].sem().to_frame(name = 'stderror_'+ metric).reset_index()
+    df_wmeans = df.groupby(['method','n'])[metric].mean().to_frame(name = 'mean_'+ metric).reset_index()
+    df_wsems = df.groupby(['method', 'n'])[metric].sem().to_frame(name = 'stderror_'+ metric).reset_index()
     df_wmeans['stderror_'+ metric]= df_wsems.loc[:, ('stderror_'+ metric)]
     return df_wmeans
 
@@ -20,25 +20,25 @@ def remove_unsolved_runs(df):
     return df[df['solution_episode'] != -1]
 
 def keep_onerow_perrun(df):
-    return df.groupby(['method','run', 'decay_multiplier']).first().reset_index()
+    return df.groupby(['method','run', 'n']).first().reset_index()
 
-def plot_solutionepisode_vs_decayrate(df):
+def plot_solutionepisode_vs_n(df):
     df=remove_unsolved_runs(df)
     df=keep_onerow_perrun(df)
     df=compute_meansandstderrors_overruns(df, metric)
-    x1= extract_axisvalues_bymethod(df,method1,'decay_multiplier')
+    x1= extract_axisvalues_bymethod(df,method1,'n')
     y1= extract_axisvalues_bymethod(df,method1,('mean_'+metric))
     error1= extract_axisvalues_bymethod(df,method1,('stderror_'+metric))
-    x2= extract_axisvalues_bymethod(df,method2,'decay_multiplier')
+    x2= extract_axisvalues_bymethod(df,method2,'n')
     y2 =extract_axisvalues_bymethod(df,method2,('mean_'+metric))
     error2 =extract_axisvalues_bymethod(df,method2,('stderror_'+metric))
     plt.style.use('seaborn-whitegrid')
-    plt.xlabel('decay multiplier')
+    plt.xlabel('n')
     plt.ylabel('solution episode') 
     plt.plot(x1, y1, 'k-')
     plt.fill_between(x1, y1-error1, y1+error1)
     #plt.plot(x2, y2, color='yellow', 'k--')
-    plt.plot(x2, y2, 'y--')
+    plt.plot(x2, y2, 'r--')
     plt.fill_between(x2, y2-error2, y2+error2)
     plt.legend(['dynasarsa', 'nstepsarsa'], loc=legend_loc)
     #plt.xlim(0,75)
@@ -46,19 +46,19 @@ def plot_solutionepisode_vs_decayrate(df):
     plt.show()
     return None
 
-def plot_nrunsolved_vs_decayrate(df):
+def plot_nrunsolved_vs_n(df):
     #Its been observed that, for the same decay rate, sometimes the run is solved, but sometimes not
     #This plot will indicate how many solved runs per decay rate
-    run_df = df.groupby(['method','run','decay_multiplier'])['solution_episode'].max().to_frame(name = 'max_solution_episode').reset_index()
+    run_df = df.groupby(['method','run','n'])['solution_episode'].max().to_frame(name = 'max_solution_episode').reset_index()
     run_df['run_solved'] = np.where(run_df['max_solution_episode'] > -1, 1, 0)
-    cnt_df = run_df.groupby(['method','decay_multiplier'])['run_solved'].sum().to_frame(name = 'nrun_solved').reset_index()
+    cnt_df = run_df.groupby(['method','n'])['run_solved'].sum().to_frame(name = 'nrun_solved').reset_index()
     
-    x1= extract_axisvalues_bymethod(cnt_df,method1,'decay_multiplier')
+    x1= extract_axisvalues_bymethod(cnt_df,method1,'n')
     y1= extract_axisvalues_bymethod(cnt_df,method1,'nrun_solved')
-    x2= extract_axisvalues_bymethod(cnt_df,method2,'decay_multiplier')
+    x2= extract_axisvalues_bymethod(cnt_df,method2,'n')
     y2 =extract_axisvalues_bymethod(cnt_df,method2,'nrun_solved')
     plt.style.use('seaborn-whitegrid')
-    plt.xlabel('decay multiplier')
+    plt.xlabel('n')
     plt.ylabel('Nruns with maze solved (out of 30)') 
     plt.plot(x1, y1, 'k-')
     plt.plot(x2, y2, 'r--')
@@ -72,12 +72,12 @@ metric= 'nrun_solved'
 legend_loc= 'upper right' 
 #best        upper right        upper left        lower left        lower right        right
 #center left        center right        lower center        upper center        center
-figurefile_header= "plots/10step_" 
+figurefile_header= "plots/ncompare_" 
 figure_file= figurefile_header + metric + ".png"
-col_names = ['method', 'run', 'episode', 'terminal_timestep', 'G_direct_mean', 'G_direct_std', 'G_direct_n',\
+col_names = ['method', 'run', 'episode', 'n', 'terminal_timestep', 'G_direct_mean', 'G_direct_std', 'G_direct_n',\
             'G_indirect_mean', 'G_indirect_std', 'G_indirect_n', 'dQ_direct_mean', 'dQ_direct_std', \
             'dQ_indirect_mean', 'dQ_indirect_std', 'alpha', 'epsilon', 'solution_episode', 'decay_multiplier']
-data_file="data/neq10.csv"
+data_file="data/test.csv"
 method1='dynasarsa'
 method2='nstepsarsa'
 
@@ -85,5 +85,5 @@ df = pd.read_csv(data_file)
 df.columns = col_names
 
 #choose appropriate function, comment out others
-plot_nrunsolved_vs_decayrate(df)
-#plot_solutionepisode_vs_decayrate(df)
+plot_nrunsolved_vs_n(df)
+#plot_solutionepisode_vs_n(df)
--- experiments/dynasarsa_vs_nstepsarsa/n/simulate_rework.py
+++ experiments/dynasarsa_vs_nstepsarsa/n/simulate_rework.py
@@ -101,7 +101,7 @@ def dynasarsa(planning_steps, run=1):
                 with open(outfile, 'a+', newline='') as csvfile:
                     writer = csv.writer(csvfile, delimiter=',',
                                             quotechar='\"', quoting=csv.QUOTE_MINIMAL)
-                    writer.writerow(['dynasarsa', run, episode, T, np.mean(G_direct_vector), np.std(G_direct_vector), len(G_direct_vector), \
+                    writer.writerow(['dynasarsa', run, episode, planning_steps, T, np.mean(G_direct_vector), np.std(G_direct_vector), len(G_direct_vector), \
                                     np.mean(G_indirect_vector), np.std(G_indirect_vector), len(G_indirect_vector), \
                                     np.mean(dQ_direct_vector), np.std(dQ_direct_vector),\
                                     np.mean(dQ_indirect_vector), np.std(dQ_indirect_vector), alpha, epsilon, solution_episode, decay_multiplier])
@@ -226,7 +226,7 @@ def nstepsarsa(n, run=1):
                 with open(outfile, 'a+', newline='') as csvfile:
                     writer = csv.writer(csvfile, delimiter=',',
                                             quotechar='\"', quoting=csv.QUOTE_MINIMAL)
-                    writer.writerow(['nstepsarsa', run, episode, T, np.mean(G_vector), np.std(G_vector), len(G_vector), \
+                    writer.writerow(['nstepsarsa', run, episode, n, T, np.mean(G_vector), np.std(G_vector), len(G_vector), \
                                     0,0,0, np.mean(dQ_vector), np.std(dQ_vector), 0, 0, alpha, epsilon, solution_episode, decay_multiplier])
                 break
             elif t >= T - 1:
@@ -308,7 +308,7 @@ if __name__ == "__main__":
     '''
     Defining the simulation related constants
     '''
-    max_episodes = 600
+    max_episodes = 400
     render_maze = False
     optimal_steps = 62
     solution_streaks = 10 #number of streaks when maze is considered solved
@@ -320,21 +320,29 @@ if __name__ == "__main__":
     env_model = []
 
     #defining experiment values
-    decay_multipliers = [0.25, 0.5, 1.0, 2.0, 4.0, 8.0] 
-    #defining outputfile
-    outfile = '../experiments/dynasarsa_vs_nstepsarsa/epsilon_decay/data/neq10.csv'
+    decay_multiplier_dynasarsa = 4
+    decay_multiplier_nstepsarsa = 1
+    dynasarsa_planningsteps = [1,2,4,8,10,16,32]
+    nstepsarsa_n = [2,4,8,10,16,32]
     
-    for decay_multiplier in decay_multipliers:
-        decay_factor = decay_multiplier*10.0/np.prod(n_states_tuple, dtype=float)
-        run=1
-        for i in range(30):
-            dynasarsa(planning_steps=10, run=run)
+    #defining outputfile
+    outfile = '../experiments/dynasarsa_vs_nstepsarsa/n/data/test.csv'
+    
+    run=1
+    decay_multiplier =  decay_multiplier_dynasarsa 
+    decay_factor = decay_multiplier*10.0/np.prod(n_states_tuple, dtype=float)
+    for i in range(30):
+        for planning_steps in dynasarsa_planningsteps:
+            dynasarsa(planning_steps=planning_steps, run=run)
             q_table = np.zeros(n_states_tuple + (n_actions,), dtype=float)
             env_model = []
             run=run+1
 
-        run=1
-        for i in range(30):
-            nstepsarsa(n=n_nstepsarsa, run=run)
+    run=1
+    decay_multiplier =  decay_multiplier_nstepsarsa
+    decay_factor = decay_multiplier*10.0/np.prod(n_states_tuple, dtype=float)
+    for i in range(30):
+        for n in nstepsarsa_n:
+            nstepsarsa(n=n, run=run)
             q_table = np.zeros(n_states_tuple + (n_actions,), dtype=float)
             run=run+1
