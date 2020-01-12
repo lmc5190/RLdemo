33a11aa427aedc084be80efb4dcae026afec99a0
Landon Chambers <landon_chambers@SAAS.LOCAL@gpu2.saas.local>
2020-01-11 18:00:41 -0600

    added experiment for alpha decay.

---
+++ experiments/dynasarsa_vs_nstepsarsa/alpha_decay/README.md
@@ -0,0 +1,39 @@
+# Epsilon Decay
+## Summary of Results
+1. For solved runs, the higher the decay multiplier the higher the sample efficiency.
+1. However, the higher decay multipliers have a lower chance of solving runs. This intuitively makes sense since decreasing the exploration rate too fast will prevent the agent from appropriately exploring the state space.
+1. For this experiment, the highest decay rates where maze is solved in every run is:
+   1. Xi = 4 for dynasarsa  (n=10)
+   1. Xi = 1 for nstepsarsa (n=10)
+
+## A simple experiment with decay rates
+
+In plot dQ_direct_std, we see dynasarsa peak and then have a long tail throughout the rest of the experiment.
+![Image of dQdirect_std](https://github.com/lmc5190/RLdemo/blob/master/experiments/dynasarsa_vs_nstepsarsa/foundation/plots/10step_dQ_direct_std.png)
+
+I'd like to conduct an experiment to see if we can shorten this tail by increasing the epsilon decay rate.
+
+I plan to plot solution time versus multiple decay rates. The choose the behavior below to govern the decay of epsilon, where t is equal to the episode number and Xi is the decay rate.
+
+![equation](https://latex.codecogs.com/gif.latex?\epsilon(t)&space;=&space;\max&space;(&space;\epsilon_{min},&space;\min(\epsilon_{max},&space;1-\log_{10}\xi&space;t)&space;))
+
+This behavior is shown graphically looks like the plot shown for epsilon versus episode number
+
+![Image of Epsilon](https://github.com/lmc5190/RLdemo/blob/master/experiments/dynasarsa_vs_nstepsarsa/foundation/plots/10step_epsilon.png)
+
+I varied Xi and observe how many runs of the 10 by 10 maze are solved by dynasarsa (planning steps = 10) and nstepsarsa (n=10).
+
+![Image of Xi Variation](https://github.com/lmc5190/RLdemo/blob/master/experiments/dynasarsa_vs_nstepsarsa/epsilon_decay/plots/10step_nrun_solved.png)
+
+Notice that dynasarsa (planning steps = 10) could handle a 4x stronger decay rate than the baseline and still solve all 30 runs. Nstepsarsa (n=10) could more than 1x the baseline value (Xi = 1) yielding multiple unsolved runs for Xi > 1.
+
+Given the sharper epsilon decay for dynasara, how much can we improve the sample efficiency? Below, I plot the solution episode number averaged over 30 runs (notice that standard error is so small, it does not even render on the graph) for each method given different epsilon decay rates. Not that any runs that did not solve the maze were omitted from the average.
+
+![Image of Xi Variation](https://github.com/lmc5190/RLdemo/blob/master/experiments/dynasarsa_vs_nstepsarsa/epsilon_decay/plots/10step_solution_episode.png)
+
+Here we say that as the decay rate goes up, solved puzzles have much higher sample efficiency. However, no higher decay rates are acceptable for nstepsarsa (n=10). Since dynasarsa (planning steps = 10) can handle a 4x higher eploration decay, we can confidently say that would yield a solved maze in ~35 episodes over ~105 episode solution for Xi = 1 (or a increase in sample efficieny by 3x).
+
+It would be interesting to test different paramters for dynasara and nstepsarsa, and see if this sensitivity remains.
+
+
+Thanks to the developers at CodeCogs https://www.codecogs.com/latex/eqneditor.php for helping me render equations. If you use their service, please acknowledge and support them!
---
+++ experiments/dynasarsa_vs_nstepsarsa/alpha_decay/plot.py
@@ -0,0 +1,89 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def compute_meansandstderrors_overruns(df, metric):
+    #returns dataframe with means and standard errors over runs for specified metric
+    #grouping done by methods and decay_multiplier
+    df_wmeans = df.groupby(['method','decay_multiplier'])[metric].mean().to_frame(name = 'mean_'+ metric).reset_index()
+    df_wsems = df.groupby(['method', 'decay_multiplier'])[metric].sem().to_frame(name = 'stderror_'+ metric).reset_index()
+    df_wmeans['stderror_'+ metric]= df_wsems.loc[:, ('stderror_'+ metric)]
+    return df_wmeans
+
+def extract_axisvalues_bymethod(df,method_name, col_name):
+    #returns vector represeting axis values from dataframe containing column 'col_name' for specified 'method_name'
+    return df[df['method']==method_name].loc[:,col_name].to_numpy()
+
+def remove_unsolved_runs(df):
+    #will remove any row where solution_episode = -1, so episodic information is removed prior to location of solved_ep
+    #this is okay since we are aggregating a single value per run over all runs, not over episodes
+    return df[df['solution_episode'] != -1]
+
+def keep_onerow_perrun(df):
+    return df.groupby(['method','run', 'decay_multiplier']).first().reset_index()
+
+def plot_solutionepisode_vs_decayrate(df):
+    df=remove_unsolved_runs(df)
+    df=keep_onerow_perrun(df)
+    df=compute_meansandstderrors_overruns(df, metric)
+    x1= extract_axisvalues_bymethod(df,method1,'decay_multiplier')
+    y1= extract_axisvalues_bymethod(df,method1,('mean_'+metric))
+    error1= extract_axisvalues_bymethod(df,method1,('stderror_'+metric))
+    x2= extract_axisvalues_bymethod(df,method2,'decay_multiplier')
+    y2 =extract_axisvalues_bymethod(df,method2,('mean_'+metric))
+    error2 =extract_axisvalues_bymethod(df,method2,('stderror_'+metric))
+    plt.style.use('seaborn-whitegrid')
+    plt.xlabel('decay multiplier')
+    plt.ylabel('solution episode') 
+    plt.plot(x1, y1, 'k-')
+    plt.fill_between(x1, y1-error1, y1+error1)
+    #plt.plot(x2, y2, color='yellow', 'k--')
+    plt.plot(x2, y2, 'y--')
+    plt.fill_between(x2, y2-error2, y2+error2)
+    plt.legend(['dynasarsa', 'nstepsarsa'], loc=legend_loc)
+    #plt.xlim(0,75)
+    plt.savefig(figure_file, format="png")
+    plt.show()
+    return None
+
+def plot_nrunsolved_vs_decayrate(df):
+    #Its been observed that, for the same decay rate, sometimes the run is solved, but sometimes not
+    #This plot will indicate how many solved runs per decay rate
+    run_df = df.groupby(['method','run','decay_multiplier'])['solution_episode'].max().to_frame(name = 'max_solution_episode').reset_index()
+    run_df['run_solved'] = np.where(run_df['max_solution_episode'] > -1, 1, 0)
+    cnt_df = run_df.groupby(['method','decay_multiplier'])['run_solved'].sum().to_frame(name = 'nrun_solved').reset_index()
+    
+    x1= extract_axisvalues_bymethod(cnt_df,method1,'decay_multiplier')
+    y1= extract_axisvalues_bymethod(cnt_df,method1,'nrun_solved')
+    x2= extract_axisvalues_bymethod(cnt_df,method2,'decay_multiplier')
+    y2 =extract_axisvalues_bymethod(cnt_df,method2,'nrun_solved')
+    plt.style.use('seaborn-whitegrid')
+    plt.xlabel('decay multiplier')
+    plt.ylabel('Nruns with maze solved (out of 30)') 
+    plt.plot(x1, y1, 'k-')
+    plt.plot(x2, y2, 'r--')
+    plt.legend(['dynasarsa', 'nstepsarsa'], loc=legend_loc)
+    #plt.xlim(0,75)
+    plt.savefig(figure_file, format="png")
+    plt.show()
+    return None
+
+metric= 'nrun_solved'
+legend_loc= 'upper right' 
+#best        upper right        upper left        lower left        lower right        right
+#center left        center right        lower center        upper center        center
+figurefile_header= "plots/10step_" 
+figure_file= figurefile_header + metric + ".png"
+col_names = ['method', 'run', 'episode', 'terminal_timestep', 'G_direct_mean', 'G_direct_std', 'G_direct_n',\
+            'G_indirect_mean', 'G_indirect_std', 'G_indirect_n', 'dQ_direct_mean', 'dQ_direct_std', \
+            'dQ_indirect_mean', 'dQ_indirect_std', 'alpha', 'epsilon', 'solution_episode', 'decay_multiplier']
+data_file="data/neq10.csv"
+method1='dynasarsa'
+method2='nstepsarsa'
+
+df = pd.read_csv(data_file)
+df.columns = col_names
+
+#choose appropriate function, comment out others
+plot_nrunsolved_vs_decayrate(df)
+#plot_solutionepisode_vs_decayrate(df)
